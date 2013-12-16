99fef530e48c5f9a77dc79c7cb1d7c2217cf0295
Hana Lee <hanalee07@gmail.com>
2013-12-16 13:03:24 -0600

    Wrote BLAST wrapper

--- wrappers.py
+++ wrappers.py
@@ -1,11 +1,23 @@
 import dna
+import os
+import subprocess
 
-def run_blast(sequences, path='/usr/bin/blastn'):
+def run_blast(sequences, db, path='', blast_type='blastn', num_hits=1, evalue=10):
+    command = [path + blast_type,
+               '-query', 'tmp.fasta',
+               '-db', db,
+               '-num_alignments', str(num_hits),
+               '-evalue', str(evalue),
+               ' -outfmt \'6 qseqid qstart qend sseqid sstart send evalue']
+    write_fasta(sequences, 'tmp.fasta')
+    blast = subprocess.Popen(command, stdout=subprocess.PIPE)
+    blast_out, blast_err = blast.communicate()
+    blast_results = [hit.split("\t") for hit in blast_out.split("\n")]
+    os.remove('tmp.fasta')
+    return blast_results
 
+def run_muscle(sequences, path=''):
     return
 
-def run_muscle(sequences, path='/usr/bin/muscle'):
-    return
-
-def run_fsa(sequences, path='/usr/bin/fsa'):
+def run_fsa(sequences, path=''):
     return
